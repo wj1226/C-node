66e3b471b9668a2d04f125b91ab4d560f5055d97
chloe <74949102+K70104@users.noreply.github.com>
2021-09-24 18:23:12 +0800

    结构体，动态内存管理实现通讯录程序
    结构体，动态内存管理实现通讯录程序

---
+++ Contact/contact/contact.h
@@ -0,0 +1,65 @@
+#pragma once
+
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+#define NAME_MAX 30
+#define SEX_MAX 15
+#define TELE_MAX 15
+#define ADDR_MAX 30
+
+#define MAX 1000
+
+#define DEFAULT_SZ 3
+
+
+struct PeoInfo
+{
+	char name[NAME_MAX];
+	int age;
+	char sex[SEX_MAX];
+	char tele[TELE_MAX];
+	char addr[ADDR_MAX];
+};
+
+
+// 通讯录
+// 1. 静态版本
+//struct Contact
+//{
+//	struct PeoInfo data[MAX]; // 存放1000个人的数据在data数组中
+//	int sz; // 记录当前通讯录有效信息的个数
+//};
+
+// 动态增长版本
+struct Contact
+{
+	struct PeoInfo* data;
+	int sz; // 通讯录中当前有效信息的个数
+	int capacity; // 通讯录当前的最大容量
+};
+
+
+// 初始化通讯录
+void InitContact(struct Contact* pc);
+
+// 增加联系人
+void AddContact(struct Contact* pc);
+
+// 显示所有联系人
+void ShowContact(struct Contact* pc);
+
+// 删除指定联系人
+void DelContact(struct Contact* pc);
+
+// 查找指定联系人
+void SearchContact(const struct Contact* pc);
+
+// 修改指定联系人
+void ModifyContact(struct Contact* pc);
+
+// 销毁通讯录
+void DestroyContact(struct Contact* pc);
