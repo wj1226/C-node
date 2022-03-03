72b4e639655fb681d330c70fba89d3d7275abcee
chloe <74949102+K70104@users.noreply.github.com>
2022-03-03 14:25:05 +0800

    更新代码
    更新代码

---
+++ 2022/test_03_02/test_03_02/test.c
@@ -0,0 +1,135 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <climits>
+
+int main()
+{
+	
+}
+
+//int main()
+//{
+//	int arr[10] = { 1,2,3 };
+//	//printf("adjflskdf");
+//
+//	int arr1[5] = { 1,2,3,4,5 };
+//	int arr2[5] = { 2,3,4,5,6 };
+//	int arr3[5] = { 3,4,5,6,7 };
+//
+//	int* p[] = { arr1, arr2, arr3 };
+//	int sz1 = sizeof(p) / sizeof(p[0]);
+//	int sz2 = sizeof(arr1) / sizeof(arr1[0]);
+//	for (int i = 0; i < sz1; i++) {
+//		for (int j = 0; j < sz2; j++) {
+//			printf("%d ", p[i][j]);
+//		}
+//		printf("\n");
+//	}
+//
+//
+//	printf("========================\n");
+//
+//	char* ret[] = { "abc", "bit", "hello" };
+//
+//	for (int i = 0; i < 3; i++) {
+//		printf("%s ", ret[i]);
+//	}
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	switch ('a') {
+//		default:
+//			printf("hello");
+//	}
+//
+//	int m = 18;
+//	int n = 24;
+//	int r = m % n;
+//	while (r != 0) {
+//		m = n;
+//		n = r;
+//		r = m % n;
+//	}
+//	printf("%d", n);
+//}
+
+//int main(void)
+//{
+//	UINT_MAX;
+//	printf("%u", 0xffffffff);
+//	int c[10] = { 0 };
+//
+//	int i, j, s;
+//	for (i = 2; i < 4; i++) 
+//		for (s = 0, j = 3; j < 5; j++) 
+//			s += i * j;
+//	printf("%d", s);
+//
+//	/*int i = 0, j = 4;
+//	while (i < j) {
+//		i++;
+//		if (i % 3 != 0) continue;
+//		j--;
+//	}
+//	printf("%d %d", i, j);*/
+//
+//	/*int i = 1;
+//	for (; i <= 50; i++) {
+//		if (i % 3 == 0 && i % 5 == 0) {
+//			break;
+//		}
+//	}
+//	printf("%d", i);*/
+//
+//	/*int i, s = 0;
+//	for (i = 1; i <= 10; i++) {
+//		s += i;
+//		if (s > 5) break;
+//	}
+//	printf("%d", s);*/
+//}
+
+//int main()
+//{
+//	int a = 5;
+//	int cnt = 0;
+//	do {
+//		a -= 2;
+//		cnt++;
+//	} while (a > 0);
+//	printf("cnt = %d a = %d\n", cnt, a);
+//}
+//
+//int main2()
+//{
+//	char a = 'a';
+//	switch (a)
+//	{
+//	case 'a'+1:
+//		printf("%d\n", a);
+//		break;
+//	default:
+//		printf("\\\\\\\\\\\\\\");
+//		break;
+//	}
+//}
+//
+//int main1()
+//{
+//	int a = 1;
+//	int b = 2;
+//	int c = 3;
+//	int d = 4;
+//	printf("%d\n", a<b ? a : c>d ? a : d);
+//
+//	int x = -9;
+//	int y = x >= 0 ? x : -x;
+//	printf("%d\n", y);
+//
+//	return 0;
+//}
+
